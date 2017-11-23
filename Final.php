545a13e867a742bcf214bd45f72b76fb7272fa90
mridu1412 <33810195+mridu1412@users.noreply.github.com>
2017-11-23 10:02:04 +0530

    Add files via upload

---
+++ Final.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Fin</title>
+	<style >
+	h1{animation-name:roll;
+		animation-timing-function: ease-in-out;
+		margin-left:30px;
+		animation-duration:2s
+
+			}
+		@keyframes roll{
+			0%{margin-left:-1200px;}
+			100% {margin-left:30px};
+
+
+
+		}
+</style>
+</head>
+<body bgcolor="black">
+<br>
+<br>
+	<h1 style="font-size:200;color:#FF4500">Thank You!!!!</h1>
+</body>
+</html>
